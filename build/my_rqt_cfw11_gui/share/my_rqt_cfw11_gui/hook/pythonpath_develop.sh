2981a1530b32420de7cc09c6a98968b272068a8d
Kevin Wang <wangykev@gmail.com>
2025-07-01 15:20:36 -0400

    Co-authored-by: karl <krypo1112@users.noreply.github.com>
    make usb recognizable everywhere

---
+++ build/my_rqt_cfw11_gui/share/my_rqt_cfw11_gui/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/ciscor/CISCOR-projects/build/my_rqt_cfw11_gui"
---
+++ build/my_rqt_speed_slider/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/ciscor/CISCOR-projects/install/my_rqt_speed_slider'
